5783f35cec991e2b7c2af7c9d5d5f086cc5b4401
Grant Toepfer <Granttpfr@gmail.com>
2015-04-29 13:32:45 -0700

    Empty model files created

---
+++ src/model/AbstractUser.java
@@ -0,0 +1,13 @@
+/*
+ * Grant Toepfer - granttoe-TCSS 360 Project 
+ */
+package model;
+
+/**
+ * @author Grant Toepfer
+ * @version Apr 29, 2015 
+ *
+ */
+public class AbstractUser {
+
+}
---
+++ src/model/Volunteer.java
@@ -0,0 +1,13 @@
+/*
+ * TCSS 360 Project - Group 3!
+ */
+package model;
+
+/**
+ * @author Grant Toepfer
+ * @version Apr 29, 2015 
+ *
+ */
+public class Volunteer extends AbstractUser {
+
+}
