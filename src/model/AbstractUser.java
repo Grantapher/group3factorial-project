a98bfaa99932379df8f5db129aaa75837ce0c6e8
mrcshaw <mrcshaw@yahoo.com>
2015-05-03 09:58:42 -0700

    with abstract user

--- src/model/AbstractUser.java
+++ src/model/AbstractUser.java
@@ -1,13 +1,49 @@
+
 /*
- * Grant Toepfer - granttoe-TCSS 360 Project 
+ * TCSS 360 Project - Group 3!
  */
 package model;
 
-/**
- * @author Grant Toepfer
- * @version Apr 29, 2015 
- *
- */
 public class AbstractUser {
+    
+    /**
+     * User Email.
+     */
+    private String myEmail;
+    
+    /**
+     * The User's first name.
+     */
+    private String myFirstName;
+    
+    /**
+     * The User's last name.
+     */
+    private String myLastName;
+
+    public AbstractUser(final String lastName, final String firstName, final String email) {
+        myEmail = email;
+        myFirstName = firstName;
+        myLastName = lastName;
+    }
+
+    public AbstractUser(final AbstractUser that) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public String getFirstName() {
+        // TODO Auto-generated method stub
+        return myFirstName;
+    }
+
+    public String getLastName() {
+        // TODO Auto-generated method stub
+        return myLastName;
+    }
+
+    public String getEmail() {
+        // TODO Auto-generated method stub
+        return myEmail;
+    }
 
 }
