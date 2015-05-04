6045bafe7fefd499a76e6fa669ad914c13a6eec6
Grant Toepfer <Granttpfr@gmail.com>
2015-05-04 13:31:06 -0700

    Fixed one unused import

--- src/model/Job.java
+++ src/model/Job.java
@@ -5,135 +5,132 @@ package model;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class Job implements Cloneable {
-	private String title;
-	private String parkName;
-	private String location;
-	private LocalDate start;
-	private LocalDate end;
-	private int curLight;
-	private int curMed;
-	private int curHeavy;
-	private String description;
-	private int maxLight;
-	private int maxMed;
-	private int maxHeavy;
-	private List<Volunteer> volunteers;
+    private final String title;
+    private final String parkName;
+    private final String location;
+    private final LocalDate start;
+    private final LocalDate end;
+    private int curLight;
+    private int curMed;
+    private int curHeavy;
+    private final String description;
+    private final int maxLight;
+    private final int maxMed;
+    private final int maxHeavy;
+    private final List<Volunteer> volunteers;
 
-	//Constructor
-	public Job(String title, String parkName, String location,
-			LocalDate start, LocalDate end, int light, int med,
-			int heavy, String description) {
-		this.title = title;
-		this.parkName = parkName;
-		this.location = location;
-		this.start = LocalDate.from(start);
-		this.end = LocalDate.from(end);
-		maxLight = light;
-		maxMed = med;
-		maxHeavy = heavy;
-		this.description = description;
-		curLight = 0;
-		curMed = 0;
-		curHeavy = 0;
-		volunteers = new ArrayList<Volunteer>();
-	}
+    // Constructor
+    public Job(final String title, final String parkName, final String location,
+            final LocalDate start, final LocalDate end, final int light, final int med,
+            final int heavy, final String description) {
+        this.title = title;
+        this.parkName = parkName;
+        this.location = location;
+        this.start = LocalDate.from(start);
+        this.end = LocalDate.from(end);
+        maxLight = light;
+        maxMed = med;
+        maxHeavy = heavy;
+        this.description = description;
+        curLight = 0;
+        curMed = 0;
+        curHeavy = 0;
+        volunteers = new ArrayList<Volunteer>();
+    }
 
+    // Getters
+    public LocalDate getStartDate() {
+        return start;
+    }
 
-	//Getters
-	public LocalDate getStartDate() {
-		return start;
-	}
+    public List<Volunteer> getVolunteers() {
+        return volunteers;
+    }
 
-	public List<Volunteer> getVolunteers() {
-		return volunteers;
-	}
+    public LocalDate getEndDate() {
+        return end;
+    }
 
-	public LocalDate getEndDate() {
-		return end;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getPark() {
+        return parkName;
+    }
 
-	public String getPark() {
-		return parkName;
-	}
+    public String getLocation() {
+        return location;
+    }
 
-	public String getLocation() {
-		return location;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    // Check if a grade is full
+    public boolean isLightFull() {
+        return maxLight == curLight;
+    }
 
+    public boolean isMedFull() {
+        return maxMed == curMed;
+    }
 
-	//Check if a grade is full
-	public boolean isLightFull() {
-		return maxLight == curLight;
-	}
+    public boolean isHeavyFull() {
+        return maxHeavy == curHeavy;
+    }
 
-	public boolean isMedFull() {
-		return maxMed == curMed;
-	}
+    /**
+     * Checks if this job has the given volunteer signed up for it.
+     */
+    public boolean containsVolunteer(final Volunteer volunteer) {
+        for (final Volunteer v : volunteers) {
+            if (v.getEmail().equals(volunteer.getEmail())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public boolean isHeavyFull() {
-		return maxHeavy == curHeavy;
-	}
+    /**
+     * Receives a list of parks as input and returns true if this job is in one
+     * of the parks.
+     */
+    public boolean isJobinParkList(final List<String> parks) {
+        for (final String p : parks) {
+            if (parkName.equals(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    // adds a volunteer to this job at the given grade
+    // returns false if the grade was full
+    public boolean addVolunteer(final Volunteer v, final char grade) {
+        if (grade == 'L' || grade == 'l') {
+            if (maxLight == curLight) {
+                return false;
+            }
+            curLight++;
+        } else if (grade == 'M' || grade == 'm') {
+            if (maxMed == curMed) {
+                return false;
+            }
+            curMed++;
+        } else if (grade == 'H' || grade == 'h') {
+            if (maxHeavy == curHeavy) {
+                return false;
+            }
+            curHeavy++;
+        }
+        volunteers.add(v);
+        return true;
+    }
 
-	/**
-	 * Checks if this job has the given volunteer signed up for it.
-	 * 
-	 */
-	public boolean containsVolunteer(Volunteer volunteer) {
-		for(Volunteer v : volunteers) {
-			if (v.getEmail().equals(volunteer.getEmail())) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/**
-	 * Receives a list of parks as input and returns true
-	 * if this job is in one of the parks.
-	 */
-	public boolean isJobinParkList(List<String> parks) {
-		for(String p : parks) {
-			if (parkName.equals(p)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	//adds a volunteer to this job at the given grade 
-	//returns false if the grade was full
-	public boolean addVolunteer(Volunteer v, char grade) {
-		if(grade == 'L' || grade == 'l') {
-			if(maxLight == curLight) {
-				return false;			
-			}
-			curLight++;
-		} else if(grade == 'M' || grade == 'm') {
-			if(maxMed == curMed) {
-				return false;
-			}
-			curMed++;
-		} else if(grade == 'H' || grade == 'h') {
-			if(maxHeavy == curHeavy) {
-				return false;
-			}
-			curHeavy++;
-		}
-		volunteers.add(v);
-		return true;
-	}
     /**
      * {@inheritDoc}
      */
