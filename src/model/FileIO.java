4ca0ff2adb186fddc7b9188ae9d1d479caa892f7
Grant Toepfer <Granttpfr@gmail.com>
2015-05-04 17:50:21 -0700

    Laid out the base FileIO Structure, waiting for Job, PM, and Admin toString() methods.

--- src/model/FileIO.java
+++ src/model/FileIO.java
@@ -3,7 +3,16 @@
  */
 package model;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,26 +22,75 @@ import java.util.Map;
  * @author Grant Toepfer
  * @version May 4, 2015
  */
-public class FileIO {
+public final class FileIO {
+    public static final char VOLUNTEER_CHAR = 'V';
+    public static final char ADMIN_CHAR = 'A';
+    public static final char PARK_MANAGER_CHAR = 'P';
+    public static final String USER_FILE = "/files/users.info";
+    public static final String JOB_FILE = "/files/jobs.info";
+    public static FileIO instance = null;
 
-    public List<User> queryUsers(final String lastName, final char userType) {
-
+    private FileIO() {
+        // empty constructor
     }
 
-    public Map<LocalDate, List<Job>> readJobs() {
-
+    public static FileIO getInstance() {
+        if (instance == null) {
+            instance = new FileIO();
+        }
+        return instance;
     }
 
-    public void appendJobs(final Job job) {
-
+    public List<User> queryUsers(final String lastName, final Character userType)
+            throws FileNotFoundException {
+        final List<User> list = new ArrayList<>();
+        final BufferedReader br = new BufferedReader(new FileReader(USER_FILE));
+        // TODO
+        try {
+            br.close();
+        } catch (final IOException theE) {
+            // ignore closing issues
+        }
+        return list;
     }
 
-    public char getUserType(final String email) {
-
+    public Map<LocalDate, List<Job>> readJobs() throws FileNotFoundException {
+        // TODO treemap?
+        final Map<LocalDate, List<Job>> map = new HashMap<>();
+        final BufferedReader br = new BufferedReader(new FileReader(JOB_FILE));
+        // TODO
+        try {
+            br.close();
+        } catch (final IOException theE) {
+            // ignore closing issues
+        }
+        return map;
     }
 
-    public void addUser(final User user) {
+    public void appendJobs(final Job job) throws IOException {
+        final PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(JOB_FILE,
+                true)));
+        pw.println(job.toString());
+        pw.close();
+    }
 
+    public char getUserType(final String email) throws FileNotFoundException {
+        final char type = 'X';
+        final BufferedReader br = new BufferedReader(new FileReader(USER_FILE));
+        // TODO
+        try {
+            br.close();
+        } catch (final IOException theE) {
+            // ignore closing issues
+        }
+        return type;
+    }
+
+    public void addUser(final User user) throws IOException {
+        final PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(USER_FILE,
+                true)));
+        pw.println(user.toString());
+        pw.close();
     }
 
 }
---
+++ src/tests/FileIOTest.java
@@ -0,0 +1,75 @@
+/*
+ * TCSS 360 Project - Group 3!
+ */
+package tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test cases for {@link model.FileIO}.
+ *
+ * @author Grant Toepfer
+ * @version May 4, 2015
+ */
+public class FileIOTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link model.FileIO#getInstance()}.
+     */
+    @Test
+    public final void testGetInstance() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for
+     * {@link model.FileIO#queryUsers(java.lang.String, java.lang.Character)}.
+     */
+    @Test
+    public final void testQueryUsers() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link model.FileIO#readJobs()}.
+     */
+    @Test
+    public final void testReadJobs() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link model.FileIO#appendJobs(model.Job)}.
+     */
+    @Test
+    public final void testAppendJobs() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link model.FileIO#getUserType(java.lang.String)}.
+     */
+    @Test
+    public final void testGetUserType() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link model.FileIO#addUser(model.User)}.
+     */
+    @Test
+    public final void testAddUser() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
