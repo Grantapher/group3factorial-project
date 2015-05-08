2a7689a2a8036d02ca625c6f1d19d267945286a2
wsp93 <wsp93@users.noreply.github.com>
2015-05-07 17:25:11 -0700

    Update and rename User.java to AbstractUser.java
    -Got rid of the constructor that takes in a User parameter...
    -Changed name back to AbstractUser and turned it into an abstract class
    -Added fields
    -Implemented constructor and getters

---
+++ src/model/AbstractUser.java
@@ -0,0 +1,29 @@
+/*
+ * TCSS 360 Project - Group 3!
+ */
+package model;
+
+public abstract class AbstractUser {
+	protected String lastName;
+	protected String firstName;
+	protected String email;
+
+    public AbstractUser(final String lastName, final String firstName, final String email) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
