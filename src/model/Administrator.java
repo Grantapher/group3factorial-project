8a64b74d688e801d5de3949b61cbb09d5334f2e9
mrcshaw <mrcshaw@yahoo.com>
2015-05-10 18:23:17 -0700

    testing

--- src/model/Administrator.java
+++ src/model/Administrator.java
@@ -1,3 +1,4 @@
+
 /*
  * TCSS 360 Project - Group 3!
  */
@@ -8,72 +9,57 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
+
 /**
  * This class represents an Administrator.
- *
+ *  
  * @author Maurice Shaw
  * @version May 2014
  */
 public final class Administrator extends AbstractUser {
-
-    /**
-     * {@inheritDoc}
-     */
-    public Administrator(final String theLastName, final String theFirstName,
-            final String theEmail) {
+    
+   /**
+    * {@inheritDoc}
+    */
+    public Administrator(final String theLastName, final String theFirstName, final String theEmail) {
         super(theLastName, theFirstName, theEmail);
     }
-
-    /**
-     * Returns a list of volunteers with a certain last name.
-     *
-     * @param theName The last name of the user to be searched.
-     * @return List of volunteers with the requested last name.
-     * @throws FileNotFoundException
-     */
+    
+   /**
+    * Returns a list of volunteers with a certain last name.
+    * 
+    * @param theName        The last name of the user to be searched.
+    * @return               List of volunteers with the requested last name.
+    * @throws FileNotFoundException 
+    */
     public List<Volunteer> findVolunteer(final String theName) throws FileNotFoundException {
-        final List<Volunteer> yourVolunteer = new ArrayList<>();
-        final List<AbstractUser> userList = FileIO.queryUsers(theName, 'V'); // List
-        // of
-        // users
-
-        if (!userList.isEmpty()) {
-
-            for (final AbstractUser theVolunteer : userList) {
-                final String volunteerLastName = theVolunteer.getLastName().toLowerCase();
-                if (volunteerLastName.equals(theName.toLowerCase())) {
+        List<Volunteer> yourVolunteer = new ArrayList<>(); 
+        List<AbstractUser> userList = FileIO.queryUsers(theName, 'V'); // List of users
+        
+        if (!userList.isEmpty()) { // list will be empty if name is not found
+            
+            for (AbstractUser theVolunteer: userList) {
+                String volunteerLastName = theVolunteer.getLastName().toLowerCase();
+                if (volunteerLastName.equals(theName.toLowerCase())) // search by last name
                     yourVolunteer.add((Volunteer) theVolunteer);
-                }
             }
         }
-        return yourVolunteer;
+        return yourVolunteer; 
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(final Object that) {
-        if (that == null) {
-            return false;
-        }
-        if (this == that) {
-            return true;
-        }
-        if (that instanceof Administrator) {
-            return super.equals(that);
-        }
-        return false;
-    }
-
+    
     /**
      * String representation of an Administrator.
-     *
+     * 
      * @return Adminstrator as a string.
      */
-    @Override
     public String toString() {
-        return "Administrator\n" + super.toString();
+        StringBuilder str = new StringBuilder();
+        str.append("Administrator: ");   
+        str.append(super.toString()); 
+        return str.toString();
     }
+    
 
 }
---
+++ src/model/AdministratorTest.java
@@ -0,0 +1,60 @@
+/*
+ * TCSS 360 Project - Group 3!
+ */
+
+package model;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Test class for an Administrator. 
+ * @author Maurice Shaw
+ * @version May 2014
+ */
+public class AdministratorTest {
+
+ // Declare objects to use in the test fixture.
+    
+    /** A park manager to use in tests. */
+    private Administrator myAdmin;
+   
+
+    /**
+     * Initialize the test fixture before each test.
+     */
+    @Before
+    public void setUp() {
+        myAdmin = new Administrator("Zues", "God of Gods", "GreekGod@thermopylae.com");
+    
+    }
+
+    /**
+     * Test method for addPark.
+     * @throws FileNotFoundException 
+     */
+    @Test
+    public void testFindVolunteer() throws FileNotFoundException {
+        
+        Volunteer vol = new Volunteer("Abraham", "Adam", "AdamAbraham@gmail.com");
+        Volunteer vol1 = new Volunteer("Adam", "Awesome", "AdamAbraham@gmail.com");
+        List<Volunteer> list = myAdmin.findVolunteer(vol.getLastName());
+        for (int x = 0; x < list.size(); x++) {
+            assertEquals(list.get(x).getLastName(), vol.getLastName());
+        }
+        List<Volunteer> empty = new ArrayList<>();
+        
+        assertEquals(myAdmin.findVolunteer(vol1.getLastName()), empty);
+        
+        
+        
+    }
+
+}
