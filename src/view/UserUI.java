6424393157a9a73d1ea9935c33b6926e2b9ab70f
Grant Toepfer <Granttpfr@gmail.com>
2015-05-18 13:18:24 -0700

    Added UserUI interface

---
+++ src/view/UserUI.java
@@ -0,0 +1,18 @@
+/*
+ * TCSS 360 Project - Group 3!
+ */
+package view;
+
+/**
+ * @author Grant Toepfer
+ * @version May 18, 2015
+ */
+public interface UserUI {
+
+    /**
+     * Opens the prompt for the user's options.
+     *
+     * @return if the user is logging out
+     */
+    public boolean userMenu();
+}
