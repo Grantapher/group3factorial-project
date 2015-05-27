b651f0fba37842f7cc778702dbb5dc4e586953a8
mrcshaw <mrcshaw@yahoo.com>
2015-05-26 20:14:01 -0700

    Fixed that ridiculousness in the isMyPark test method

--- src/tests/ParkManagerTest.java
+++ src/tests/ParkManagerTest.java
@@ -3,24 +3,25 @@
  */
 package tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import model.ParkManager;
 
 import org.junit.Before;
 import org.junit.Test;
 
+
 /**
- * Test class for a Park Manager
- * 
+ * Test class for a Park Manager 
  * @author Maurice Shaw
  * @version May 2014
  */
 public class ParkManagerTest {
-
+    
     // Declare objects to use in the test fixture.
-
+    
     /** A park manager to use in tests. */
     private ParkManager myManager1;
+   
 
     /**
      * Initialize the test fixture before each test.
@@ -28,7 +29,7 @@ public class ParkManagerTest {
     @Before
     public void setUp() {
         myManager1 = new ParkManager("GodOfWar", "Cratos", "GreekGod@thermopylae.com");
-
+    
     }
 
     /**
@@ -36,25 +37,48 @@ public class ParkManagerTest {
      */
     @Test
     public void testaddPark() {
-        final String[] arr = { "GOOD", "BETTER", "BEST", "BESTEST", "" };
-
+        String[] arr = {"GOOD", "BETTER", "BEST", "BESTEST", ""};
+        
         for (int x = 0; x < arr.length; x++) {
             myManager1.addPark(arr[x]);
-            if (x > 3) {
-                assertEquals(myManager1.getParks().size(), arr.length - 1);
-            } else {
+            if (x > 3) // accepts only strings that are at east one character long
+                assertEquals(myManager1.getParks().size(), (arr.length - 1));               
+            else //add park to list as needed
                 assertEquals(myManager1.getParks().get(x), arr[x]);
-            }
-
-        }
-
-        final int size = myManager1.getParks().size();
-        final String shouldFail = "";
-        myManager1.addPark(shouldFail);// should not add, size should stay the
-                                       // same
-        assertEquals("A job with no characters has been added", myManager1.getParks().size(),
-                size);
-
+            
+        }  
+        
+        int size = myManager1.getParks().size();
+        String shouldFail = "";
+        myManager1.addPark(shouldFail);// should not add park, size should stay the same
+        assertEquals("A job with no characters has been added", myManager1.getParks().size(), size);
+        
+        
     }
+  
+    /**
+     * Test method for isMyPark.
+     */
+    @Test
+    public void testisMyPark() {
+        String[] arr = {"GOOD", "BETTER", "BEST", "BESTEST", ""};
+        
+        for (int x = 0; x < arr.length; x++) {
+            myManager1.addPark(arr[x]);
+            if (x < 4) // accepts only strings that are at east one character long
+               assertTrue(myManager1.isMyPark(arr[x]));               
+            else //add park to list as needed
+                assertFalse(myManager1.isMyPark(arr[x]));
+            
+        }  
+        
+        int size = myManager1.getParks().size();
+        String shouldFail = "";
+        myManager1.addPark(shouldFail);// should not add, size should stay the same
+        assertEquals("A job with no characters has been added", myManager1.getParks().size(), size);
+        
+        
+    }
+    
 
 }
